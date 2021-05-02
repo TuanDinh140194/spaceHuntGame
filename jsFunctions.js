05eefd26b9f297f37081e3be58b86b298b57bcec
Travis Wisch <82557021+Wisch-Tea@users.noreply.github.com>
2021-05-01 19:38:14 -0700

    Move Update
    Should be complete; though minor changes will likely be needed depending on how 'custom game settings' is implemented

--- jsFunctions.js
+++ jsFunctions.js
@@ -5,13 +5,14 @@
 <!---->
 function initializeState()
 {
-        alert("Initializing!");
-        document.forms[0].location.value="0,0";
-        document.forms[0].energy.value=1000;
-        document.forms[0].supplies.value=100.00
-        document.forms[0].credits.value=1000;
-        document.forms[0].message.value="[NULL]";
-
+    alert("Initializing!");
+    document.forms[1].location.value="0,0";
+    document.forms[1].energy.value=1000;
+    document.forms[1].supplies.value=100.00
+    document.forms[1].credits.value=1000;
+    document.forms[1].message.value="[NULL]";
+	document.forms[1].mapSizeX.value=128;
+	document.forms[1].mapSizeY.value=128;
 }
 
 
@@ -22,34 +23,67 @@ function initializeState()
 function makeMove(direction)
 {
 
-        var coords = document.forms[0].location.value.split(',');
+        var coords = document.forms[1].location.value.split(',');
         var x = Number(coords[0]);
         var y = Number(coords[1]);
 
         switch(direction)
         {
                 case '0':
-                        x += 1;
+                        if (x >= document.forms[1].mapSizeX.value) {
+							x = 0; // Reset via wormhole behavior.
+						}
+						else {x += parseInt(document.forms[0].distance.value);}
                         break;
                 case '90':
-                        y += 1;
+						if (y >= document.forms[1].mapSizeY.value) {
+							y = 0; // Reset via wormhole behavior.
+						}
+						else {y += parseInt(document.forms[0].distance.value);}
                         break;
                 case '180':
-                        x -= 1;
+						if (x < 1) {
+							x = 128; // Wormhole to other side of h-plane
+						}
+						else {x -= parseInt(document.forms[0].distance.value);}
                         break;
                 case '270':
-                        y -= 1;
+						if (y < 1) {
+							y = 128; // Wormhole to other side of v-plane
+						}
+						else {y -= parseInt(document.forms[0].distance.value);}
                         break;
         }
 
 
 	checkLocation()
-        document.forms[0].location.value = x.toString() + "," + y.toString();
+        document.forms[1].location.value = x.toString() + "," + y.toString();
 
 
-                          // update the fields on the web page with the results
-        document.forms[0].energy.value -= 10;
-        document.forms[0].supplies.value -= 2;
-        document.forms[0].message.value=item = "Movement Detected";
-	checkStatus();
+        // update the fields on the web page with the results
+		document.forms[1].energy.value -= 10 * parseInt(document.forms[0].distance.value);
+		document.forms[1].supplies.value -= 2 * parseInt(document.forms[0].distance.value)// Two percent for later: (document.forms[1].supplies.value * 0.02);
+		switch(direction){
+			case '0':
+				document.forms[1].message.value=item = "Moving Eastbound " + parseInt(document.forms[0].distance.value) + " time(s).";
+				break;
+			case '90':
+				document.forms[1].message.value=item = "Moving Northbound " + parseInt(document.forms[0].distance.value) + " time(s).";
+				break;
+			case '180':
+				document.forms[1].message.value=item = "Moving Westbound " + parseInt(document.forms[0].distance.value) + " time(s).";
+				break;
+			case '270':
+				document.forms[1].message.value=item = "Moving Southbound " + parseInt(document.forms[0].distance.value) + " time(s).";
+				break;
+		}
+		
+	checkStatus()
+		if (document.forms[1].energy.value <= 0){
+			alert("Game Over! You ran out of energy!");
+			setTimeout("location.reload(true);", 500); // Half a second after clearing alert the page will refresh.
+		}else if (document.forms[1].supplies.value <= 0){
+			alert("Game Over! You ran out of supplies!");
+			setTimeout("location.reload(true);", 500); // Half a second after clearing alert the page will refresh.
+		}
 }
