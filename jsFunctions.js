f172bd5abe6e5383d954c26f35a3772625e535b5
henguyen-99 <81539116+henguyen-99@users.noreply.github.com>
2021-05-03 20:49:04 -0700

    Add files via upload

--- jsFunctions.js
+++ jsFunctions.js
@@ -31,57 +31,35 @@ function makeMove(direction)
         {
                 case '0':
                         if (x >= document.forms[1].mapSizeX.value) {
-							x = 0; // Reset via wormhole behavior.
+							x = 0 + (parseInt(document.forms[0].distance.value) - 1); // Reset via wormhole behavior.
 						}
 						else {x += parseInt(document.forms[0].distance.value);}
                         break;
                 case '90':
 						if (y >= document.forms[1].mapSizeY.value) {
-							y = 0; // Reset via wormhole behavior.
+							y = 0 + (parseInt(document.forms[0].distance.value) - 1); // Reset via wormhole behavior.
 						}
 						else {y += parseInt(document.forms[0].distance.value);}
                         break;
                 case '180':
 						if (x < 1) {
-							x = 128; // Wormhole to other side of h-plane
+							x = 128 - (parseInt(document.forms[0].distance.value) - 1); // Wormhole to other side of h-plane
 						}
 						else {x -= parseInt(document.forms[0].distance.value);}
                         break;
                 case '270':
 						if (y < 1) {
-							y = 128; // Wormhole to other side of v-plane
+							y = 128 - (parseInt(document.forms[0].distance.value) - 1); // Wormhole to other side of v-plane
 						}
 						else {y -= parseInt(document.forms[0].distance.value);}
                         break;
         }
 
-<<<<<<< HEAD
-<!---->
-function checkLocation() {
-    document.forms[1].location.value = x.toString() + "," + y.toString();
-=======
->>>>>>> parent of d6d7e6c (Add files via upload)
 
 	checkLocation()
         document.forms[1].location.value = x.toString() + "," + y.toString();
 
 
-<<<<<<< HEAD
-<!---->
-function checkStatus() {
-	//Checks if ship still has energy and supplies to continue playing the game.
-	//If either run out, should alert the player, signifying end of game.
-	//Function should only run if gamemode is set to "regular play".
-	if (document.forms[1].energy.value <= 0) {
-		alert("Game Over! You ran out of energy!");
-		setTimeout("location.reload(true);", 500); // Half a second after clearing alert the page will refresh.
-	}
-	
-	else if (document.forms[1].supplies.value <= 0) {
-		alert("Game Over! You ran out of supplies!");
-		setTimeout("location.reload(true);", 500); // Half a second after clearing alert the page will refresh.
-	}
-=======
         // update the fields on the web page with the results
 		document.forms[1].energy.value -= 10 * parseInt(document.forms[0].distance.value);
 		document.forms[1].supplies.value -= 2 * parseInt(document.forms[0].distance.value)// Two percent for later: (document.forms[1].supplies.value * 0.02);
@@ -101,9 +79,6 @@ function checkStatus() {
 		}
 		
 	checkStatus()
-		//Checks if ship still has energy and supplies to continue playing the game.
-		//If either run out, should alert the player, signifying end of game.
-		//Function should only run if gamemode is set to "regular play".
 		if (document.forms[1].energy.value <= 0){
 			alert("Game Over! You ran out of energy!");
 			setTimeout("location.reload(true);", 500); // Half a second after clearing alert the page will refresh.
@@ -111,5 +86,4 @@ function checkStatus() {
 			alert("Game Over! You ran out of supplies!");
 			setTimeout("location.reload(true);", 500); // Half a second after clearing alert the page will refresh.
 		}
->>>>>>> parent of d6d7e6c (Add files via upload)
 }
