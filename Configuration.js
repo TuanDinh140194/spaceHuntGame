702be64c5cc5d06e52c66106b0a361e4427f0db2
Tri Le <trilq1402@gmail.com>
2021-05-08 05:48:54 -0700

    fix bug 2

--- Configuration.js
+++ Configuration.js
@@ -522,16 +522,6 @@ if (localStorage.getItem("configuration") !== null) {
 }
 
 
-<<<<<<< HEAD
-=======
-document.getElementById("mapSizeX").value = configuration[0].value.x;
-document.getElementById("mapSizeY").value = configuration[0].value.y;
-document.getElementById("energy").value = configuration[1].value;
-document.getElementById("supplies").value = configuration[5].value;
-document.getElementById("credits").value = configuration[2].value;
-document.getElementById("location").value = "0,0";
-
->>>>>>> 2f6f9507ddb4d4a69015808c8fcf33a91d4a50ca
 
 var buttonConfig = document.createElement("button");
 buttonConfig.innerHTML = "Configuration";
@@ -554,14 +544,8 @@ function exitConfiguration() {
     document.getElementById("mapSizeX").value = configuration[0].value.x;
     document.getElementById("mapSizeY").value = configuration[0].value.y;
     document.getElementById("energy").value = configuration[1].value;
-<<<<<<< HEAD
-    document.getElementById("supplies").value = configuration[2].value;
+	document.getElementById("supplies").value = configuration[2].value;
     document.getElementById("credits").value = configuration[5].value;
-=======
-    document.getElementById("supplies").value = configuration[5].value;
-    document.getElementById("credits").value = configuration[2].value;
-    document.getElementById("location").value = "0,0";
->>>>>>> 2f6f9507ddb4d4a69015808c8fcf33a91d4a50ca
     formConfig.style.display = "none";
 }
 
@@ -572,6 +556,6 @@ function whenSubmit() {
     document.getElementById("supplies").value = configuration[2].value;
     document.getElementById("credits").value = configuration[5].value;
 
-}
+}s
 
 
