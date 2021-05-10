4d481850e401c713fad759636452ca5e6b121831
mridenour7 <82553574+mridenour7@users.noreply.github.com>
2021-05-10 15:14:03 -0700

    Updated Sensors

--- Sensors.js
+++ Sensors.js
@@ -15,53 +15,38 @@ sensorsButton.addEventListener("click", function () {
     const y = Number(coords[1]);
     SIZE1 = document.getElementById("mapSizeX").value
     SIZE2 = document.getElementById("mapSizeY").value
-    let ycel = SIZE2;
-    const detected = [...Array(SIZE)].map(_ => Array(SIZE).fill(0))
     var found = false; // found something
 
-    for (let j = x-3; j <= x+1; j++) {
+    for (let j = x-2; j <= x+2; j++) {
         if (j < 0 || j >= SIZE1) { // don't try sensing coordinates that are out of bounds
             continue;
         }
-        for (let i = y-3; i <= y+1; i++) {
+        for (let i = y-2; i <= y+2; i++) {
             if (i < 0 || i >= SIZE2) {
                 continue;
             }
-            ycel = SIZE2 - i - 1; // the y index of celestial array goes from top to bottom
-            //cell = $id("cell-" + j + "-"+ i);
-            //if(cell != null) {
-            //    type = cell.getAttribute("type") + j + " , " + i;
-            //}
-            if (celestial[ycel][j] != 0) {
-                //detected[i][j] = celestial[i][j];
+            cell = $id("cell-" + j + "-"+ i);
+            if(cell != null) {
+                type = cell.getAttribute("type"); 
                 found = true;
+                cell.style.display = "block";
 
-                if (celestial[ycel][j] == Planets) {
-                    alert("Detected: Planet at " + parseInt(j+1) + "," + parseInt(i+1));
+                if (type == "planet") {
+                    alert("Detected: Planet at " + j + "," + i);
                 }
-                else if (celestial[ycel][j] == Asteroids) {
-                    alert("Detected: Asteroid at " + parseInt(j+1) + "," + parseInt(i+1));
+                else if (type == "asteroid") {
+                    alert("Detected: Asteroid at " + j + "," + i);
                 }
-                else if (celestial[ycel][j] == SpaceStations) {
-                    alert("Detected: SpaceStation at " + parseInt(j+1) + "," + parseInt(i+1));
+                else if (type == "space-station") {
+                    alert("Detected: SpaceStation at " + j + "," + i);
                 }
-                else if (celestial[ycel][j] == Wormholes) {
-                    alert("Detected: Wormhole at " + parseInt(j+1) + "," + parseInt(i+1));
+                else if (type == "wormhole") {
+                    alert("Detected: Wormhole at " + j + "," + i);
                 }
 
             }
         }
     }
-//    if (found) {
-//        let table = '';
-//        for (let i = 0; i < SIZE; i++){
-//            for(let j = 0; j < SIZE; j++){
-//                table += (detected[i][j]) + "|"
-//            }
-//            table += '\n'
-//        }
-//        console.log('\n\n' + table);
-//    }
   
     if (!found) {
         alert("Sensors deployed: nothing detected");
