95c64b7fa5649246844fd19c413e65074a6375ac
Tri Le <trilq1402@gmail.com>
2021-05-11 19:17:35 -0700

    fix bug

--- Map.js
+++ Map.js
@@ -1,9 +1,12 @@
 class Map {
 
-    constructor(id, className ,width, height, configuration, ship,celestial) {
+    constructor(id, className, width, height, configuration, ship, celestial) {
 
         this.width = width;
         this.height = height;
+
+        this.boundx = 0;
+        this.boundy = 0;
 
         this.id = id;
 
@@ -13,22 +16,22 @@ class Map {
         this.elem = document.createElement('div');
         this.elem.className = className;
 
-        if ( ship != undefined)
+        if (ship != undefined)
             this.ship = ship;
 
     }
 
     render() {
-       
+
 
         this.elem.style.zIndex = "0";
         this.elem.setAttribute("id", this.id);
-        
+
         if (this.ship !== undefined)
             this.elem.style.position = "fixed";
-        
-            this.elem.style.transition = "all 1s linear 0s";
-            document.body.appendChild(this.elem);
+
+        this.elem.style.transition = "all 1s linear 0s";
+        document.body.appendChild(this.elem);
 
         if (this.ship !== undefined) {
             this.elem.appendChild(this.ship.elem);
@@ -57,54 +60,55 @@ class Map {
                 cell.style.margin = 0;
                 cell.style.border = "1px solid red";
                 cell.style.textAlign = "center";
-                cell.setAttribute("id", "td-" + ( j +1 ) + "-" + (this.height -i ));
-                switch(this.celestial[i][j]){
+                cell.setAttribute("id", "td-" + (j + 1) + "-" + (this.height - i));
+                switch (this.celestial[i][j]) {
                     case 1: {
                         //Planet
                         var img = document.createElement("img");
-                        img.style.width = (CELL_SIZE-15) + "px";
-                        img.style.height = (CELL_SIZE-15) + "px";
+                        img.style.width = (CELL_SIZE - 15) + "px";
+                        img.style.height = (CELL_SIZE - 15) + "px";
                         img.style.display = "none";
                         img.setAttribute("src", "img/planet.png");
                         img.setAttribute("type", "planet");
-                        img.setAttribute("id", "cell-" + ( j +1 ) + "-" + (this.height -i ));
+                        img.setAttribute("id", "cell-" + (j + 1) + "-" + (this.height - i));
                         cell.appendChild(img);
-                    }break;
+                    } break;
                     case 2: {
                         //asteroid
                         var img = document.createElement("img");
-                        img.style.width = (CELL_SIZE-15) + "px";
-                        img.style.height = (CELL_SIZE-15) + "px";
+                        img.style.width = (CELL_SIZE - 15) + "px";
+                        img.style.height = (CELL_SIZE - 15) + "px";
                         img.style.display = "none";
                         img.setAttribute("src", "img/asteroid.png");
                         img.setAttribute("type", "asteroid");
-                        img.setAttribute("id", "cell-" + (j + 1) + "-" + (this.height -i));
+                        img.setAttribute("id", "cell-" + (j + 1) + "-" + (this.height - i));
                         cell.appendChild(img);
-                    }break;
+                    } break;
                     case 3: {
                         //space-station
                         var img = document.createElement("img");
-                        img.style.width = (CELL_SIZE-15) + "px";
-                        img.style.height = (CELL_SIZE-15) + "px";
+                        img.style.width = (CELL_SIZE - 15) + "px";
+                        img.style.height = (CELL_SIZE - 15) + "px";
                         img.style.display = "none";
                         img.setAttribute("src", "img/space-station.png");
                         img.setAttribute("type", "space-station");
                         img.setAttribute("id", "cell-" + (j + 1) + "-" + (this.height - i));
                         cell.appendChild(img);
-                    }break;
+                    } break;
                     case 4: {
                         //wormholes
                         var img = document.createElement("img");
-                        img.style.width = (CELL_SIZE-15) + "px";
-                        img.style.height = (CELL_SIZE-15) + "px";
+                        img.style.width = (CELL_SIZE - 15) + "px";
+                        img.style.height = (CELL_SIZE - 15) + "px";
                         img.style.display = "none";
                         img.setAttribute("src", "img/wormholes.png");
                         img.setAttribute("type", "wormhole");
-                        img.setAttribute("id", "cell-" + (j + 1) + "-" + (this.height - i ));
+                        img.setAttribute("id", "cell-" + (j + 1) + "-" + (this.height - i));
                         cell.appendChild(img);
-                    }break;
+                    } break;
                 }
                 row.appendChild(cell);
+
             }
             table.appendChild(row);
         }
@@ -119,44 +123,57 @@ class Map {
     }
 
     moveSpaceShip(x, y, angle) {
-        
+
         let message = "";
 
-        let cell = $id("cell-" + x + "-"+ (this.height - y +1));
-        
-        if (cell !== null) {
-            cell.style.display = "block";
-            message =  cell.getAttribute("type");
+        //Calculate cell size for the first time 
+        if (this.boundx === 0) {
+            let cell2 = $id("td-" + 1 + "-" + 1);
+            if (cell2 !== null) {
+                this.boundx = cell2.getBoundingClientRect().width;
+                this.boundy = cell2.getBoundingClientRect().height;
+                this.ship.boundx = this.boundx;
+                this.ship.boundy = this.boundy;
+            }
+            else {
+                this.boundx = CELL_SIZE;
+                this.boundy = CELL_SIZE;
+            }
         }
 
-        if ($id("cell-"+this.ship.x_of_map+ "-"+ (this.height - this.ship.y_of_map + 1)) === null
-        && $id("start-"+this.ship.x_of_map+ "-"+ (this.height - this.ship.y_of_map + 1)) === null
-        )
-        {
-            let td = $id("td-"+this.ship.x_of_map+ "-"+ (this.height - this.ship.y_of_map + 1) );
+        //check whether (x,y) has an artifact
+        let cell = $id("cell-" + x + "-" + (this.height - y + 1));
+        if (cell !== null) {
+            cell.style.display = "block";
+            message = cell.getAttribute("type");
+        }
+
+        //Show glow start for the passing step.
+        if ($id("cell-" + this.ship.x_of_map + "-" + (this.height - this.ship.y_of_map + 1)) === null
+            && $id("start-" + this.ship.x_of_map + "-" + (this.height - this.ship.y_of_map + 1)) === null
+        ) {
+            let td = $id("td-" + this.ship.x_of_map + "-" + (this.height - this.ship.y_of_map + 1));
             if (td !== null) {
                 let img = document.createElement("img");
-                img.style.width = (CELL_SIZE-25) + "px";
-                img.style.height = (CELL_SIZE-25) + "px";
+                img.style.width = (CELL_SIZE - 25) + "px";
+                img.style.height = (CELL_SIZE - 25) + "px";
                 img.setAttribute("src", "img/glow_start.png");
-                img.setAttribute("id", "start-" + this.ship.x_of_map+ "-"+ (this.height - this.ship.y_of_map + 1));
+                img.setAttribute("id", "start-" + this.ship.x_of_map + "-" + (this.height - this.ship.y_of_map + 1));
                 td.appendChild(img);
             }
-    
-          //  console.log("td-"+this.ship.x_of_map+ "-"+ (this.height - this.ship.y_of_map + 1));
         }
 
         this.ship.setLocation(x, y, angle);
 
-        this.elem.style.left = (window.innerWidth / 2 - x * CELL_SIZE) + "px";
-        this.elem.style.top = (window.innerHeight / 2 - y * CELL_SIZE) + "px";
+        this.elem.style.left = (window.innerWidth / 2 - x * this.boundx) + "px";
+        this.elem.style.top = (window.innerHeight / 2 - y * this.boundy) + "px";
 
         return message;
     }
 }
 
 
-var ship ;
-var renderMap ;
+var ship;
+var renderMap;
 
 
