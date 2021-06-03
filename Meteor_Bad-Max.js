27f0cee0f609114c58a98cc89779c641990d23e6
Travis Wisch <82557021+Wisch-Tea@users.noreply.github.com>
2021-06-02 18:32:51 -0700

    Bad-Max_Update
    Bad Max story implementation.

--- Meteor_Bad-Max.js
+++ Meteor_Bad-Max.js
@@ -11,12 +11,33 @@ document.querySelectorAll("button.movement").forEach(item => {
 		const y = Number(coords[1]);
 		var cell = $id("cell-" + x + "-" + y);
 		if (cell == null) { // The current cell is free space, so an encounter can occur.
-			// Getting the value 3 has the 1 in 35 chance to encounter a meteor storm in free space.
-			if (Math.round(Math.random() * 35) == 3) {
+			// Getting the value 23 has the 1 in 50 chance to encounter a meteor storm in free space.
+			if (Math.round(Math.random() * 50) == 23) {
 				// Meteor Storm occurred, so now the ship is damaged.
 				if (document.forms[1].shipStatus.value == "Fully Operational") {
 					document.forms[1].shipStatus.value = "Damaged";
 					alert("You've encountered a meteor storm and as a result your ship is damaged and needs to be fixed at a repair depot!\n(Energy is consumed at 5 times its normal rate)");
+					return;
+				}
+			}
+			// Getting the value 3 has the 1 in mapSizeX chance to encounter Bad Max and his crew in free space.
+			// if (true) { FOR TESTING QUICKLY
+			if (Math.round(Math.random() * document.forms[1].mapSizeX.value) == 3) {
+				// One of three possibilities
+				var chanceVar = Math.round(Math.random() * 2);
+				if (chanceVar == 0) {
+					document.forms[1].message.value = "Bad Max Encountered! [Loss - Crew Death]";
+					gameOver("You and your crew have encountered Bad Max and his henchmen who know you're in search of the Koca-Kola recipe he stole!\nDespite your best efforts, you along with your crew have been killed..");
+				}
+				else if (chanceVar == 1) {
+					document.forms[1].message.value = "Bad Max Encountered! [Victory - No Damage]";
+					alert("You and your crew have encountered Bad Max and his henchmen who know that you're in search of the Koca-Kola recipe he stole!\nHe attempted to board your ship and kill your crew, but with clever tactics and quick maneuvering you've safely escaped!");
+				}
+				else {
+					document.forms[1].message.value = "Bad Max Encountered! [1/2 Supplies 0 Credits]";
+					document.forms[1].credits.value = 0;
+					document.forms[1].supplies.value = Math.round(document.forms[1].supplies.value / 2);
+					alert("You and your crew have encountered Bad Max and his henchmen who know that you're in search of the Koca-Kola recipe he stole!\nThey managed to board your ship, overcome your crew, steal all your credits and half of the supplies!");
 				}
 			}
 		}
